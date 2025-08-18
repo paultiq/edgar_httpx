0c4dc7bc6deaa48a5a7624cab1471371b0c72236
paul@iqmo.com <paul@iqmo.com>
2025-08-18 09:13:13 -0400

    dont use a huge file for testing

--- tests/test_single_file.py
+++ tests/test_single_file.py
@@ -2,14 +2,17 @@ from httpxthrottlecache import HttpxThrottleCache
 import time
 import logging
 logger = logging.getLogger(__name__)
-LARGE_FILE_URL = "https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip"
+#
+# This is too large... expensive test for a GHA RunnerLARGE_FILE_URL = "https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip"
  
+FILE_URL = "https://www.sec.gov/Archives/edgar/data/51143/000155837021009351/ibm-20210630ex32157e1d5.htm"
+
 def test_large_file(manager_cache: HttpxThrottleCache):
     with manager_cache.http_client() as client:
 
         logger.info("Making first request")
         start = time.perf_counter()
-        r1 = client.get(LARGE_FILE_URL)
+        r1 = client.get(FILE_URL)
         assert r1.headers.get("x-cache") == "MISS" or r1.extensions.get("from_cache") == False
 
         end = time.perf_counter()
@@ -21,7 +24,7 @@ def test_large_file(manager_cache: HttpxThrottleCache):
 
         logger.info("Making second request")
         start = time.perf_counter()
-        r2 = client.get(LARGE_FILE_URL)
+        r2 = client.get(FILE_URL)
         assert r2.headers.get("x-cache") == "HIT" or r2.extensions.get("from_cache") == True
 
         end = time.perf_counter()
