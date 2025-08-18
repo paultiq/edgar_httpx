402d7fbe8ae0ba2b8123e3ff9aec84001342f447
paul@iqmo.com <paul@iqmo.com>
2025-08-18 08:58:42 -0400

    more generous timing

--- tests/test_large_file.py
+++ tests/test_large_file.py
@@ -1,26 +1,33 @@
 from httpxthrottlecache import HttpxThrottleCache
 import time
-
+import logging
+logger = logging.getLogger(__name__)
 LARGE_FILE_URL = "https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip"
  
 def test_large_file(manager_cache: HttpxThrottleCache):
     with manager_cache.http_client() as client:
+
+        logger.info("Making first request")
         start = time.perf_counter()
-        response = client.get(LARGE_FILE_URL)
+        r1 = client.get(LARGE_FILE_URL)
+        assert r1.headers.get("x-cache") == "MISS" or r1.extensions.get("from_cache") == False
+
         end = time.perf_counter()
 
-        assert response.status_code == 200
+        assert r1.status_code == 200
         first_duration = end - start
 
         assert 5 < first_duration < 120 # this is a rough estimate to make sure the file took about as long as it should
 
+        logger.info("Making second request")
         start = time.perf_counter()
-        response = client.get(LARGE_FILE_URL)
+        r2 = client.get(LARGE_FILE_URL)
+        assert r2.headers.get("x-cache") == "HIT" or r2.extensions.get("from_cache") == True
+
         end = time.perf_counter()
 
-        assert response.status_code == 200
+        assert r2.status_code == 200
         second_duration = end - start
 
-        assert second_duration < 8 # this is a rough estimate
-
+        assert second_duration < 20 # generous time
     
